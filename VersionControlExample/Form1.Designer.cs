8ac2db74e5e0332729e56c6d1cf71cec5099c218
ismailsoftdev <ismailibrahimabdinur143@gmail.com>
2023-10-10 11:48:28 +0300

    Added the UI to the project

--- VersionControlExample/Form1.Designer.cs
+++ VersionControlExample/Form1.Designer.cs
@@ -28,12 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtFullName = new TextBox();
+            txtBoxAddress = new TextBox();
+            SaveBtn = new Button();
+            LblResult = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(250, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(219, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Version Control Example";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(126, 98);
+            label2.Name = "label2";
+            label2.Size = new Size(125, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Enter your name";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(126, 158);
+            label3.Name = "label3";
+            label3.Size = new Size(140, 21);
+            label3.TabIndex = 2;
+            label3.Text = "Enter your address";
+            // 
+            // txtFullName
+            // 
+            txtFullName.Location = new Point(303, 96);
+            txtFullName.Name = "txtFullName";
+            txtFullName.Size = new Size(343, 23);
+            txtFullName.TabIndex = 3;
+            // 
+            // txtBoxAddress
+            // 
+            txtBoxAddress.Location = new Point(303, 160);
+            txtBoxAddress.Name = "txtBoxAddress";
+            txtBoxAddress.Size = new Size(343, 23);
+            txtBoxAddress.TabIndex = 4;
+            // 
+            // SaveBtn
+            // 
+            SaveBtn.Location = new Point(349, 224);
+            SaveBtn.Name = "SaveBtn";
+            SaveBtn.Size = new Size(135, 45);
+            SaveBtn.TabIndex = 5;
+            SaveBtn.Text = "Save";
+            SaveBtn.UseVisualStyleBackColor = true;
+            // 
+            // LblResult
+            // 
+            LblResult.AutoSize = true;
+            LblResult.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            LblResult.Location = new Point(235, 325);
+            LblResult.Name = "LblResult";
+            LblResult.Size = new Size(0, 25);
+            LblResult.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(LblResult);
+            Controls.Add(SaveBtn);
+            Controls.Add(txtBoxAddress);
+            Controls.Add(txtFullName);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtFullName;
+        private TextBox txtBoxAddress;
+        private Button SaveBtn;
+        private Label LblResult;
     }
 }
